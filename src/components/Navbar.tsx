0f7f4115b7a6e90c99117a9ff2f1f60cf5055454
Leandro Hosken <lshv04@gmail.com>
2026-03-10 14:55:35 -0300

    starting aplication and creating front page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-[#7695EC] h-16 flex items-center px-8">
+      <span className="text-white text-xl font-bold">CodeLeap Network</span>
+    </nav>
+  );
+}
